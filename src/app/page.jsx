cb56fd9906ad739d1e02b692e5381a0841c4fc83
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-08-07 13:26:51 +0300

    Add body section

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      {/* Home header */}
+      <HomeHeader />
+      
+
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image width='300' height='100' src='/google.png' alt="Google Image" />
+        <HomeSearch/>
+      </div>
+    </>
+  );
+}
